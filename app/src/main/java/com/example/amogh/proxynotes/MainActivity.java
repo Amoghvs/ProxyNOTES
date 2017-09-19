82882a8587723a6e9e129bec9923ac9bc4ace0da
amoghvs <amoghvs@gmail.com>
2017-09-19 09:19:48 +0530

    Final

--- app/src/main/java/com/example/amogh/proxynotes/MainActivity.java
+++ app/src/main/java/com/example/amogh/proxynotes/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get the layout from video_main.xml
+
         setContentView(R.layout.activity_main);
 
 
